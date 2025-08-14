3d3d2db5cb6ec482eade9b94dab9cc0f19308c59
Alesso <alessogoldodiego@gmail.com>
2025-08-14 11:20:15 -0600

    Primeras funcionalidades

---
+++ Practica1-ARCH/src/main/java/com/mycompany/practica1/arch/Practica1ARCH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1.arch;
+
+/**
+ *
+ * @author alesso
+ */
+public class Practica1ARCH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
